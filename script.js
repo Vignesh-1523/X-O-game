e2c709dc3abad47f3c3ce24cc1fab5f04d46ee6d
Vignesh-1523 <gabrielmac0912@gmail.com>
2024-03-17 09:03:11 -0700

    modified

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let block = document.querySelectorAll('span')
 let btn = document.querySelector('button')
+let h3 = document.querySelector('h3')
+let p = document.querySelector('p')
 
 let possibilities = [
     [0, 1, 2],
@@ -17,14 +19,21 @@ function giveValue() {
     block.forEach((cell, index) => {
         cell.addEventListener('click', () => {
             if (cell.innerHTML == "" && count % 2 == 0) {
+                h3.innerHTML = " -> Turn"
+                p.innerHTML = "O"
+                p.style.color = "rgb(1, 212, 167)"
                 cell.innerHTML = "×"
                 count++
                 cell.style.color = "rgb(226, 223, 12)"
                 cell.style.fontSize = "4.8em"
             } else if (cell.innerHTML == "" && count % 2 != 0) {
+                h3.innerHTML = " -> Turn"
+                p.innerHTML = "X"
+                p.style.color = "rgb(226, 223, 12)"
                 cell.innerHTML = "〇"
                 count++
                 cell.style.color = "rgb(1, 212, 167)"
+                cell.style.fontSize = "2.5em"
             } else {
                 alert("This field is already occupied")
             }
@@ -52,6 +61,8 @@ function reset() {
     ]
     block.forEach(e => {
         e.innerHTML = ""
+        p.innerHTML = ""
+        h3.innerHTML = "X / O"
     })
 }
 
@@ -72,23 +83,31 @@ function checkWinner(index, content) {
         }
         if (x == 3) {
             setTimeout(() => {
-                alert('X wins')
-                reset()
+                // alert('X wins')
+                // reset()
+                h3.innerHTML = ' wins ! "'
+                p.innerHTML = '" X'
+                p.style.color = "rgb(226, 223, 12)"
             }, 300)
         } else if (o == 3) {
             setTimeout(() => {
-                alert('O wins')
-                reset()
+                // alert('O wins')
+                // reset()
+                h3.innerHTML = ' wins ! "'
+                p.innerHTML = '" O'
+                p.style.color = "rgb(1, 212, 167)"
             }, 300)
         } else {
             console.log(arr)
         }
         // console.log("x : " + x + ", " + "o : " + o)
     })
-    if(count == 9){
+    if (count == 9) {
         setTimeout(() => {
-            alert('Match Tie')
-            reset()
+            h3.innerHTML = "Match Tie"
+            p.innerHTML = ""
+            // alert('Match Tie')
+            // reset()
             // console.log(count);
         }, 300)
     }
